28906c2acfc860607738b6a5bcd09c2d809bfca6
swapnil-sh <swapnil.sharma2709@gmail.com>
2022-03-22 00:05:10 +0530

    added currency calculation service

---
+++ currency-calculation-service/src/main/java/com/microservice/currencycalculation/CurrencyCalculationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.currencycalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCalculationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCalculationServiceApplication.class, args);
+	}
+
+}
---
+++ currency-calculation-service/src/test/java/com/microservice/currencycalculation/CurrencyCalculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.currencycalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyCalculationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
